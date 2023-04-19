de67a946b4bd27617d027e2eb764549fb20c93e1
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-18 19:50:06 -0700

    Complete showWeather function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -19,10 +19,15 @@ document
 function showWeather(evt) {
   evt.preventDefault();
 
-  const url = "/weather.json";
   const zipcode = document.querySelector("#zipcode-field").value;
+  const queryString = new URLSearchParams({ zipcode: zipcode }).toString();
+  const url = `/weather.json?${queryString}`;
 
-  // TODO: request weather with that URL and show the forecast in #weather-info
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseData) => {
+      document.querySelector("#weather-info").innerHTML = responseData.forecast;
+    });
 }
 
 document.querySelector("#weather-form").addEventListener("submit", showWeather);
