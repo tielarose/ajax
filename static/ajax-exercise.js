d2f9e4c8a856802c65b3b9e55fb9a02bb49627c0
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-18 19:32:51 -0700

    Complete showFortune function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -1,25 +1,31 @@
-'use strict';
+"use strict";
 
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-  // TODO: get the fortune and show it in the #fortune-text div
+  fetch("/fortune")
+    .then((response) => response.text())
+    .then((responseData) => {
+      document.querySelector("#fortune-text").innerHTML = responseData;
+    });
 }
 
-document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
+document
+  .querySelector("#get-fortune-button")
+  .addEventListener("click", showFortune);
 
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
   evt.preventDefault();
 
-  const url = '/weather.json';
-  const zipcode = document.querySelector('#zipcode-field').value;
+  const url = "/weather.json";
+  const zipcode = document.querySelector("#zipcode-field").value;
 
   // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
-document.querySelector('#weather-form').addEventListener('submit', showWeather);
+document.querySelector("#weather-form").addEventListener("submit", showWeather);
 
 // PART 3: ORDER MELONS
 
@@ -29,4 +35,4 @@ function orderMelons(evt) {
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
-document.querySelector('#order-form').addEventListener('submit', orderMelons);
+document.querySelector("#order-form").addEventListener("submit", orderMelons);
